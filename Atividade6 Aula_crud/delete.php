cf21899fb1930498b9a154cff41d60cde4837cab
milton01031997 <167126597+milton01031997@users.noreply.github.com>
2025-11-17 16:36:20 -0300

    Add files via upload

---
+++ Atividade6 Aula_crud/delete.php
@@ -0,0 +1,17 @@
+<?php
+// excluir.php
+require_once 'conexao.php'; // Inclui a conexão
+// --- LÓGICA DO DELETE (D) ---
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM usuarios WHERE id = ?";
+     $stmt = $pdo->prepare($sql);
+
+     $stmt->execute([$id]);
+}
+// Redireciona de volta para o index.php
+header("Location: index.php");
+exit;
+?>
+
